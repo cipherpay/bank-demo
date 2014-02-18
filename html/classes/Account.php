ee5a661dd6d58e5e7b5d37da8de83ea8f234af6f
josh@halfnibble.com <josh@halfnibble.com>
2014-02-17 19:56:07 -0800

    finished Account Class

--- html/classes/Account.php
+++ html/classes/Account.php
@@ -52,29 +52,65 @@
 
 		// Link to a customer
 		public function addCustomer($customer_id, $permission = "primary") {
-
+			$sql = "INSERT INTO customer_account VALUES($this->id, $customer_id, '$permission');";
+			$result = $this->db->query($sql);
+			if ($result && $this->db->affected_rows > 0)
+				return true;
+			else
+				return false;
 		}
 
 		// Unlink customer
 		public function removeCustomer($customer_id) {
-
+			$sql = "DELETE FROM customer_account WHERE account_id = $this->id AND customer_id = $customer_id;";
+			$result = $this->db->query($sql);
+			if ($result && $this->db->affected_rows > 0)
+				return true;
+			else
+				return false;
 		}
 
 		// Remove all customer links and delete transactions
 		private function unlinkAll() {
+			$sql = "DELETE FROM customer_account WHERE account_id = $this->id;";
+			$result = $this->db->query($sql);
 
+			$sql2 = "DELETE FROM transactions WHERE account_id = $this->id;";
+			$result2 = $this->db->query($sql2);
+
+			if ($result && $result2)
+				return true;
+			else
+				return false;
 		}
 
 		// Permanently remove account
 		public function remove() {
-
+			// Remove all foreign key references:
+			$ready = $this->unlinkAll();
+			// Then remove the account
+			if ($ready) {
+				$sql = "DELETE FROM accounts WHERE account_id = $this->id;";
+				$result = $this->db->query($sql);
+				if ($result && $this->db->affected_rows > 0)
+					return true;
+				else
+					return false;
+			} else {
+				echo "Error! Failed to unlink customers or transactions.";
+				exit();
+			}
 		}
 
 		// Static function to add new account
 		// Account::add(database, balance, account type, account name)
 		public static function add($db, $balance = 0.00, $type = 1, $name) {
 			$sql = "INSERT INTO accounts VALUES(NULL, $balance, $type, '$name');";
-			// ...
+			$result = $db->query($sql);
+			if ($result)
+				return $db->insert_id;
+			else
+				return "ERROR";
 		}
 
 		// Static function to list all accounts
